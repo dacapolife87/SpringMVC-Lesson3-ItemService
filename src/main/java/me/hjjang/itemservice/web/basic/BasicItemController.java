76f8e9691a691b0e719e87fcfd32c60a2f0fc696
HyungjuJang <dacapolife87@gmail.com>
2021-05-01 09:39:26 +0900

    feat : 수정용 controller 추가

--- src/main/java/me/hjjang/itemservice/web/basic/BasicItemController.java
+++ src/main/java/me/hjjang/itemservice/web/basic/BasicItemController.java
@@ -38,11 +38,11 @@ public class BasicItemController {
         return "basic/addForm";
     }
 
-//    @PostMapping("/add")
+    //    @PostMapping("/add")
     public String addItemV1(@RequestParam String itemName,
-                       @RequestParam int price,
-                       @RequestParam Integer quantity,
-                       Model model) {
+                            @RequestParam int price,
+                            @RequestParam Integer quantity,
+                            Model model) {
         Item item = new Item();
         item.setItemName(itemName);
         item.setPrice(price);
@@ -55,9 +55,9 @@ public class BasicItemController {
         return "basic/item";
     }
 
-//    @PostMapping("/add")
+    //    @PostMapping("/add")
     public String addItemV2(@ModelAttribute("item") Item item,
-                       Model model) {
+                            Model model) {
 
         itemRepository.save(item);
 
@@ -66,7 +66,7 @@ public class BasicItemController {
         return "basic/item";
     }
 
-//    @PostMapping("/add")
+    //    @PostMapping("/add")
     public String addItemV3(@ModelAttribute Item item) {
         // Item -> item (첫글자를 소문자로 바꿈) ModelAttribute에서 name 생략시 생략시
         itemRepository.save(item);
@@ -80,6 +80,19 @@ public class BasicItemController {
         return "basic/item";
     }
 
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+
+        return "basic/editForm";
+    }
+
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+        itemRepository.update(itemId, item);
+        return "redirect:/basic/items/{itemId}";
+    }
 
 
     /**
