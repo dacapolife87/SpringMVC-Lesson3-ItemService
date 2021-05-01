9e9d2d554d520d8ab534d320e96572efc015b10c
HyungjuJang <dacapolife87@gmail.com>
2021-05-01 11:15:23 +0900

    feat : RedirectAttribute의 도입 RedirectAttribute에 원하는 값을 넣어주어 Redirect된 페이지에서 처리되도록한다 리다이렉트의 url에 해당 속성키값이 있으면 해당 자리에 대입해주고 아니면 쿼리파라미터 형식으로 보낸다

--- src/main/java/me/hjjang/itemservice/web/basic/BasicItemController.java
+++ src/main/java/me/hjjang/itemservice/web/basic/BasicItemController.java
@@ -6,6 +6,7 @@ import me.hjjang.itemservice.domain.item.ItemRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -80,11 +81,18 @@ public class BasicItemController {
         return "basic/item";
     }
 
-    @PostMapping("/add")
+//    @PostMapping("/add")
     public String addItemV5(Item item) {
-        // Item -> item (첫글자를 소문자로 바꿈) ModelAttribute에서 생략시 생략시 item객체에 담김
         itemRepository.save(item);
         return "redirect:/basic/items/"+item.getId();
+    }
+
+    @PostMapping("/add")
+    public String addItemV6(Item item, RedirectAttributes redirectAttributes) {
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/basic/items/{itemId}";
     }
 
     @GetMapping("/{itemId}/edit")
